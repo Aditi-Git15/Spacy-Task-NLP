8a2d67d10740a7642f450c16d9ab9cb72c5516ed
Aditi-Git15 <sonaraditi96@gmail.com>
2025-06-12 22:56:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from location_Extractor import extract_locations
 
 def run_cli():
-    print("Location Extractor")
+    print("Location Extractor using Spacy")
     while True:
         user_input = input("\nEnter a sentence (or type 'exit' to quit):\n> ")
         if user_input.lower() == "exit":
@@ -10,7 +10,7 @@ def run_cli():
         if locations:
            print(f" Extracted Locations: {locations}")
         else:
-            print("No locations found.")
+            print("No locations found!!")
 
 if __name__ == "__main__":
     run_cli()
